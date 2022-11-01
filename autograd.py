Fixed square derivative, changed dfs, added stopping caclculating gradient for variables which do not need it, added visualizations, trains

autograd.py
@@ -145,15 +145,20 @@ class Tensor(Node):
         Calculates the "local" partial derivatives of this function, 
         for common unary operations like unary sum (including single operand case), mean and square
         """
-        if self.operation == "mean" or self.operation == "sum" or self.operation == "square":
-            op1 = self.parents[0]
-            normalizer = 1. if self.operation == "sum" else (
-                1. / op1.value.size if self.operation == "mean" else 2.)
+        implemented_ops = ["mean", "sum", "square"]
+        if self.operation not in implemented_ops:
+            raise Exception(f"Unary op {self.operation} not implemented")
+
+        op1 = self.parents[0]
+        if self.operation == "square":
+            self.local_grad = {
+                op1.id: 2. * op1.value
+            }
+        else:
+            normalizer = 1. if self.operation == "sum" else 1. / op1.value.size
             self.local_grad = {
                 op1.id: normalizer * np.ones(op1.value.shape)
             }
-        else:
-            raise Exception(f"Unary op {self.operation} not implemented")
 
     def calc_local_gradient(self):
         """
@@ -186,19 +191,21 @@ class Tensor(Node):
             else:
                 raise Exception(f"Op {self.operation} not implemented")
 
-    def backward(self, clear=False):
+    def backward(self):
         """
         Runs backprop, returns dict of the Tensor names as dict
         """
         gradient = {}
         # TODO vector-valued output, multiply parent with child
 
-        def dfs(node: Tensor, accumulated_product=np.array(1.)):
+        def dfs(node: Tensor):
             node.calc_local_gradient()
             for parent in node.parents:
+                # Skip calculation of gradients for nodes
+                if not parent.is_variable:
+                    continue
                 # HINT: We sum over the partial derivatives of a vector-valued intermediate function to obtain the gradient
-
-                a = accumulated_product.flatten()
+                a = node.grad.flatten()
                 b_old_shape = tuple(node.local_grad[parent.id].shape)
                 b = node.local_grad[parent.id].flatten()
                 start_ = time.perf_counter()
@@ -214,19 +221,15 @@ class Tensor(Node):
                 print(f"Grad product of for d{node.name}/{parent.name} between shape {a.shape} and {b.shape}, calc took " + "%.2f" %
                       ((end_ - start_)*1000.) + "ms")
                 """
+                if parent.grad is None:
+                    parent.grad = 0.
+                parent.grad += grad_elem
+                gradient[parent.name] = grad_elem
                 if parent.operation:
-                    dfs(parent, grad_elem)
-                else:
 
-                    # Set the gradient, terminate search
-                    if clear:
-                        parent.grad = None
-                    else:
-                        if parent.grad is None:
-                            parent.grad = 0.
-                        parent.grad += grad_elem
-                        gradient[parent.name] = grad_elem
+                    dfs(parent)
 
+        self.grad = np.array(1.)
         dfs(self)
         return gradient
 
@@ -424,7 +427,7 @@ def draw_node_result_labels(nx_graph, node_positions, size):
     def op_node_label(node: Tensor):
         value_str = "%.2f" % node.value if isinstance(
             node.value, float) else str(node.value)
-        return f"{node.name}={value_str}"
+        return f"{node.name}={value_str}, grad: {node.grad}"
     result_labels = {node: op_node_label(attributes["ag_tensor"])
                      for node, attributes in nx_graph.nodes(data=True)}
 

function_approximation.py
@@ -37,22 +37,26 @@ class GradientDescent:
         # TODO get grad from self.params.grad after refactoring
         for name, param in self.params.items():
             grad = param.grad
-            print(f"Grad for param {name} is {grad}")
+            #print(f"Grad for param {name} is {grad}")
             np.subtract(param.value, grad * self.lr, out=param.value)
 
 
 def train():
 
     interval = [-5, 5.]
-    x_vals = np.linspace(*interval, num=10)
+    x_vals = np.linspace(*interval, num=20)
     y_vals = np.vectorize(f)(x_vals)
 
-    intermediate_feat = 5
-    model = Sequential([
+    intermediate_feat = 2
+    seq_model = Sequential([
         Linear(in_features=1, out_features=intermediate_feat),
         ReLu(),
         Linear(in_features=intermediate_feat, out_features=1)
     ])
+
+    linear_model = Linear(in_features=1, out_features=1)
+    lin_relu = Sequential([Linear(in_features=1, out_features=1), ReLu()])
+    model = seq_model
 
     # Convert the train vector of from shape (N,) to (N, 1), this is the correct batch shape
     x_train = [Tensor(np.array(x_i).reshape(1), f"x_{i}",
@@ -65,7 +69,7 @@ def train():
 
     params = model.get_parameters()
     print(f"Params are: {params}")
-    optimizer = GradientDescent(params, lr=0.001)
+    optimizer = GradientDescent(params, lr=0.0004)
     loss = mse_loss
 
     # TODO workaround, fix properly
@@ -73,19 +77,30 @@ def train():
 
     def forward_x_train():
         y_pred = []
-        for x_i, y_true in zip(x_train, y_train):
+        for x_i in x_train:
             y_pred.append(model.forward(x_i))
         return y_pred
 
-    plt.plot(x_vals, y_vals)
-    y_pred = forward_x_train()
-    plt.plot(x_vals, [t.value for t in y_pred])
-    plt.title("Before training, function")
-    plt.legend()
-    plt.xlim(tuple(interval))
-    plt.show()
+    def plot_model_vs_function():
+        plt.plot(x_vals, y_vals)
+        y_pred = forward_x_train()
+        y_scalars = [t.value for t in y_pred]
+        plt.plot(x_vals, y_scalars)
+        plt.title("Function vs model")
+        plt.legend()
+        plt.xlim(tuple(interval))
+        plt.show()
 
-    for i in range(1000):
+    def print_parameters():
+        for param_id, param in params.items():
+            print(f"Param {param.name}({param_id}) is: {param.value}")
+
+    plot_model_vs_function()
+
+    last_loss = None
+
+    loss_vals = []
+    for i in range(10000):
         # TODO workaround, fix properly
         if id_counts is not None:
             Node.id_cnt, Tensor.id_cnt = id_counts
@@ -101,22 +116,37 @@ def train():
             else:
                 loss += residual
 
-            draw_computation_graph(loss)
+            """ optimizer.zero_grad()
+            loss.backward()
+            draw_computation_graph(loss) """
+
         if id_counts is None:
             id_counts = (Tensor.id_cnt, Node.id_cnt)
         #loss = mse_loss(y_train, y_pred)
 
-        print(f"Loss is: {loss}")
+        if (i % 1000) == 0:
+            print(f"Iteration #{i} Loss is: {loss}")
+
+        if last_loss is not None:
+            if np.abs(last_loss-loss.value) < 0.0001:
+                break
+
+        last_loss = loss.value
+        loss_vals.append(last_loss)
 
         optimizer.zero_grad()
-
         loss.backward()
         optimizer.step()
 
-    plt.plot(x_vals, y_vals)
-    y_pred = model.forward(x_vals)
-    plt.plot(x_vals, y_pred)
-    plt.xlim(tuple(interval))
+        # print_parameters()
+
+        # plot_model_vs_function()
+
+    plot_model_vs_function()
+
+    plt.plot(np.arange(len(loss_vals)), loss_vals)
+    plt.title("Loss")
+    plt.show()
 
 
 train()