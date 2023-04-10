Fixed backprop bug, trains now

autograd.py
@@ -175,7 +175,7 @@ class Tensor(Node):
         elif len(self.parents) == 2:
             self.calc_local_grad_binary_ops()
 
-    def backward(self):
+    def backward(self, trace=False, profile=False):
         """
         Runs backprop, returns dict of the Tensor names as dict
         """
@@ -185,26 +185,34 @@ class Tensor(Node):
         def dfs(node: Tensor):
             node.calc_local_gradient()
             for parent in node.parents:
-                # Skip calculation of gradients for nodes
+                # Skip calculation of gradients for nodes for which we should not calculate the gradient
                 if not parent.is_variable:
                     continue
-                # HINT: We sum over the partial derivatives of a vector-valued intermediate function to obtain the gradient
+
                 a = node.grad.flatten()
                 b_old_shape = tuple(node.local_grad[parent.id].shape)
                 b = node.local_grad[parent.id].flatten()
                 start_ = time.perf_counter()
-                if a.size > 1:
-                    grad_elem = np.sum(a, axis=-1) * b
-                else:
-                    grad_elem = a * b
+                # HINT: We sum over the partial derivatives of a vector-valued intermediate function to obtain the gradient
+                # TODO whyy ?? I think if we have a rank > 1 of the tensor was meant here. I commented it out as it triggers
+                # for rank 1 tensor as size is then 20
+                # if a.size > 1:
+                #    grad_elem = np.sum(a, axis=-1) * b
+                # else:
+                grad_elem = a * b
                 end_ = time.perf_counter()
-                # Reshape back to be able to subtract the wheights in gradient descent directly
+                # Reshape back to be able to subtract the weights in gradient descent directly
                 grad_elem = grad_elem.reshape(b_old_shape)
 
-                """
-                print(f"Grad product of for d{node.name}/{parent.name} between shape {a.shape} and {b.shape}, calc took " + "%.2f" %
-                      ((end_ - start_)*1000.) + "ms")
-                """
+                if trace:
+
+                    print(
+                        f"Grad product of for d{node.name}/{parent.name} is:\n{grad_elem}")
+
+                if profile:
+                    print(f"Grad calc between shape {a.shape} and {b.shape}, calc took " + "%.2f" % (
+                        (end_ - start_)*1000.) + "ms")
+
                 if parent.grad is None:
                     parent.grad = 0.
                 parent.grad += grad_elem
@@ -352,6 +360,11 @@ def build_networkx_graph(root_node: Tensor):
 
 
 def draw_computation_graph(root_tensor: Tensor, size=1.):
+    """
+    Draw the computation graph for a given root tensor
+    Opens the window
+    """
+
     nx_graph = build_networkx_graph(root_tensor)
 
     def node_type_to_color(node: Tensor):

func_approx_torch.py
@@ -77,7 +77,7 @@ def plot_model_vs_function(model, x_t: Tensor, y_t: Tensor, x_lim):
     plt.show()
 
 
-def train_loop(dataloader, model, loss_fn, optimizer):
+def train_loop(dataloader, model, loss_fn, optimizer, trace=False):
     losses = []
     for batch_i, batch in enumerate(dataloader):
 
@@ -87,13 +87,30 @@ def train_loop(dataloader, model, loss_fn, optimizer):
 
         X, y = batch
         # Compute prediction and loss
+
+        if trace:
+            print(f"Params before update:\n")
+            for name, param in model.named_parameters():
+                print(f"Param {name}: {param}")
+
         pred = model(X)
         loss = loss_fn(pred, y)
 
         # Backpropagation
         optimizer.zero_grad()
         loss.backward()
+
+        if trace:
+            print(f"Grad after grad calc:\n")
+            for name, param in model.named_parameters():
+                print(f"Grad:{param.grad}")
+
         optimizer.step()
+
+        if trace:
+            print(f"Params after update:\n")
+            for name, param in model.named_parameters():
+                print(f"Param {name}: {param}")
 
         losses.append(loss.item())
     return losses
@@ -101,20 +118,23 @@ def train_loop(dataloader, model, loss_fn, optimizer):
 
 def train():
 
-    interval = [-6, 5.]
+    interval = [-6, 4.5]
 
     intermediate_feat = 20
     model = NeuralNetwork(intermediate_layers=intermediate_feat)
 
-    dataset = FunctionApproximationDataset(f, interval, sample_size=100)
+    batch_size = 20
+    sample_size = 5 * batch_size
+    dataset = FunctionApproximationDataset(
+        f, interval, sample_size=sample_size)
 
-    dataloader = DataLoader(dataset, batch_size=20,
+    dataloader = DataLoader(dataset, batch_size=batch_size,
                             shuffle=True, num_workers=0)
 
     params = model.parameters()
 
     #optimizer = Adam(params, lr=.01)
-    optimizer = SGD(params, lr=.005, momentum=.1)
+    optimizer = SGD(params, lr=1e-3)
     mse_loss = nn.MSELoss()
 
     # Move to cuda
@@ -125,14 +145,13 @@ def train():
     #plot_model_vs_function(model, dataset.x_vals, dataset.y_vals, interval)
 
     loss_vals = []
-    epochs = 1000
+    epochs = 2000
     for epoch_i in range(epochs):
         loss_vals += train_loop(dataloader, model, mse_loss, optimizer)
 
-        if (epoch_i % 1000) == 0:
-            print(f"Params are: ")
-            for param in list(params):
-                print(f"Param: {param}")
+        print(f"Epoch #{epoch_i} loss is: {loss_vals[-1]}")
+        if (epoch_i % 100) == 0:
+
             plot_model_vs_function(model, dataset.x_vals,
                                    dataset.y_vals, interval)
 

function_approximation.py
@@ -84,7 +84,7 @@ def train():
 
     interval = [-6, 4.5]
 
-    batch_size = 5
+    batch_size = 20
     sample_size = 5 * batch_size
     x_vals_orig = np.linspace(*interval, num=sample_size)
 
@@ -111,7 +111,7 @@ def train():
     params = model.get_parameters()
     params_str = ', '.join(map(str, params.values()))
     print(f"Params are:\n{params_str}")
-    optimizer = GradientDescent(params, lr=1e-6)
+    optimizer = GradientDescent(params, lr=1e-3)
     loss = mse_loss
 
     # TODO workaround, fix properly
@@ -174,9 +174,9 @@ def train():
                 else:
                     loss += residual
 
-                """ optimizer.zero_grad()
-                loss.backward()
-                draw_computation_graph(loss) """
+            # normalize loss
+            # 72.
+            loss = loss * Tensor(np.array(1./batch_size), None, False, None)
 
             if id_counts is None:
                 id_counts = (Tensor.id_cnt, Node.id_cnt)
@@ -194,12 +194,13 @@ def train():
 
             optimizer.zero_grad()
             loss.backward()
+            # draw_computation_graph(loss)
             optimizer.step(trace=False)
 
             # print_parameters()
 
         #optimizer.lr *= lr_decay
-        if (epoch_i % 10) == 0:
+        if (epoch_i % 100) == 0:
             plot_model_vs_function(x_orig_t, y_orig_t)
 
     plot_model_vs_function(x_orig_t, y_orig_t)